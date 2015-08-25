59f15dd46e3a64a2b3e9a0f52620fba142889b83
ppowalowski <patrik@powalowski.de>
2015-08-25 17:32:58 +0200

    added support for multiple values per declaration

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ function rem2pxPlugin(size) {
   function rem2px(style) {
     visit(style, function(declarations) {
       declarations.forEach(function(d) {
-        if(/([0-9]+)\.?([0-9]*)\s*rem/.test(d.value)) {
-          d.value = parseFloat(d.value, 10)*size + 'px';
+        if(/\d*\.?\d+rem/.test(d.value)) {
+		  d.value = d.value.replace(/(\d*\.?\d+)rem/g,function(match,m1){
+			return parseFloat(m1, 10)*size + 'px';
+		  });
         }
       });
     });
   }
-
   return rem2px;
 }
 
 module.exports = rem2pxPlugin;
-
